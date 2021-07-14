91f9ce3a7242ce582dba0dd0052c76555e5176c8
gaoyuanyuan2 <823789010@qq.com>
2021-07-14 23:35:24 +0800

    3sum

--- src/main/java/com/leetcode/demo/array/L015ThreeSum.java
+++ src/main/java/com/leetcode/demo/array/L015ThreeSum.java
@@ -25,4 +25,34 @@ public class L015ThreeSum {
         }
         return list;
     }
+
+    public static void main(String[] args) {
+        new L015ThreeSum().myThreeSum(new int[]{-1,0,1,2,-1,-4});
+    }
+    public List<List<Integer>> myThreeSum(int[] nums) {
+        List<List<Integer>> resList = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return resList;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int now = -nums[i];
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (now == sum) {
+                    resList.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[i] == nums[++l]) ;
+                    while (l < r && nums[r] == nums[--r]) ;
+                } else if (now > sum) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return resList;
+    }
 }
